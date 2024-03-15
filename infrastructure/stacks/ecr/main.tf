69acc613a38982f4b5251fb7549c9242b4a03f8b
Konrad Bloor <konrad.bloor1@nhs.net>
2024-03-15 15:30:40 +0000

    Update terraform to add policy

--- infrastructure/stacks/ecr/main.tf
+++ infrastructure/stacks/ecr/main.tf
@@ -10,3 +10,31 @@ resource "aws_ecr_repository" "sfrunner_repo" {
     encryption_type = "AES256"
   }
 }
+
+resource "aws_ecr_repository_policy" "policy" {
+  repository = aws_ecr_repository.sfrunner_repo.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": [
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:PutImage",
+        "ecr:InitiateLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:CompleteLayerUpload"
+      ]
+    }
+  ]
+}
+EOF
+}
